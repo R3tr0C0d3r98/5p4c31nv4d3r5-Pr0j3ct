17e9519423d6d4a0975bcbd7611dfdc41a2f21e7
dastudent <dastudent@cn18.home>
2020-04-03 16:16:35 +0100

    second commit with express and most css

---
+++ Public/javascript/app.js
@@ -0,0 +1,19 @@
+// const scoreBoard = document.querySelector(".score");
+// const lives = document.querySelector(".lives");
+// const levelUp = document.querySelector(".level")
+// const invaders = document.querySelectorAll('alien')
+// const player = document.querySelector(".human")
+// const projectile = document.querySelector(".lazer1")
+// const projectile = document.querySelector(".lazer2")
+// let lives = 3;
+// let score = 0;
+// let level = 1;
+
+// //spawn in sprites and start the game
+// function startGame(){
+//     scoreBoard.textContent = 0;
+//     level.textContent = 1
+//     lives.textContent = 3
+
+// }
+
